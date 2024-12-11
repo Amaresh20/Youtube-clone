74a8faf27fb18b271900f55ccaa3714bdc7c65cd
Amaresh20 <sahooa218@gmail.com>
2024-12-11 20:27:55 +0530

    working on filter functionality

--- frontend/youtube/src/components/Home.jsx
+++ frontend/youtube/src/components/Home.jsx
@@ -5,32 +5,40 @@ function Home() {
   const [homeData, setHomeData] = useState(youtubeData);
 
   return (
-    <div className="grid-container grid grid-cols-3 gap-4 mx-5">
-      {homeData.map((data) => {
-        return (
-          <div key={data.id} className=" card h-[420px] hover:cursor-pointer">
-            <img
-              src={data.thumbnailUrl}
-              alt=""
-              srcset=""
-              className=" thumbnail h-[300px] w-[100%] rounded-lg hover:rounded-none"
-            />
-            <p className=" card-title text-xl font-bold font-sans">
-              {data.title}
-            </p>
-            <p className="card-info text-sm text-gray-600 font-sans">
-              {data.uploader}
-            </p>
-            <p className="card-info text-sm text-gray-600 font-sans">
-              {data.views}
-            </p>
-            <p className="card-info text-sm text-gray-600 font-sans">
-              {data.uploadDate}
-            </p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="flex flex-col">
+        <h1 className="bg-red-400">filter button</h1>
+        <div className="grid-container grid grid-cols-3 gap-4 mx-5">
+          {homeData.map((data) => {
+            return (
+              <div
+                key={data.id}
+                className=" card h-[420px] hover:cursor-pointer"
+              >
+                <img
+                  src={data.thumbnailUrl}
+                  alt=""
+                  srcset=""
+                  className=" thumbnail h-[300px] w-[100%] rounded-lg hover:rounded-none"
+                />
+                <p className=" card-title text-xl font-bold font-sans">
+                  {data.title}
+                </p>
+                <p className="card-info text-sm text-gray-600 font-sans">
+                  {data.uploader}
+                </p>
+                <p className="card-info text-sm text-gray-600 font-sans">
+                  {data.views}
+                </p>
+                <p className="card-info text-sm text-gray-600 font-sans">
+                  {data.uploadDate}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 export default Home;
